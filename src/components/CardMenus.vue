4ceb0a89c3604a1fc8111778a8934548ba73edea
Praditya <pradityaaldi.s@gmail.com>
2022-11-30 14:17:39 +0700

    update

--- src/components/CardMenus.vue
+++ src/components/CardMenus.vue
@@ -2,14 +2,29 @@
   <div class="card-menu p-3 rounded d-flex justify-content-between mb-3">
     <div class="d-flex flex-column justify-content-between">
       <div>
-        <h3 class="fw-bold mb-0">Air Mineral</h3>
-        <p class="mb-0">600 ml</p>
+        <h3 class="fw-bold mb-0">{{ item.title }}</h3>
+        <p class="mb-0">{{ item.desc }}</p>
       </div>
-      <p class="mb-0">5.400</p>
+      <p class="mb-0">{{ item.price }}</p>
     </div>
     <div class="d-flex flex-column align-items-end">
       <img class="img-product rounded mb-2" src="/img/dummy.jpg" alt="dummy" />
-      <button class="btn btn-menu btn-outline-primary">tambah</button>
+      <button
+        v-if="showButton"
+        @click="tambah"
+        class="btn btn-menu btn-outline-primary"
+      >
+        tambah
+      </button>
+      <div v-else class="d-flex align-items-center gap-3">
+        <button class="btn btn-transparent" @click="kurang">
+          <img src="/img/icon-minus.svg" alt="minus" />
+        </button>
+        <p class="mb-0">{{ item.selected }}</p>
+        <button class="btn btn-transparent" @click="tambah">
+          <img src="/img/icon-add.svg" alt="add" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +32,30 @@
 <script>
 export default {
   name: "CardMenus",
+  props: ["item"],
+  data() {
+    return {
+      showButton: true,
+    };
+  },
+  methods: {
+    tambah() {
+      this.$emit("tambah", this.item.id);
+      if (this.item.selected == 0) {
+        this.showButton = true;
+      } else {
+        this.showButton = false;
+      }
+    },
+    kurang() {
+      this.$emit("kurang", this.item.id);
+      if (this.item.selected == 0) {
+        this.showButton = true;
+      } else {
+        this.showButton = false;
+      }
+    },
+  },
 };
 </script>
 
--- src/views/MenusView.vue
+++ src/views/MenusView.vue
@@ -1,32 +1,56 @@
 <template>
   <NavbarCompVue />
   <main class="container py-5">
-    <div class="card-menu p-3 rounded d-flex justify-content-between">
-      <div class="d-flex flex-column justify-content-between">
-        <div>
-          <h3 class="fw-bold mb-0">Air Mineral</h3>
-          <p class="mb-0">600 ml</p>
-        </div>
-        <p class="mb-0">5.400</p>
-      </div>
-      <div class="d-flex flex-column align-items-end">
-        <img
-          class="img-product rounded mb-2"
-          src="/img/dummy.jpg"
-          alt="dummy"
-        />
-        <button class="btn btn-menu btn-outline-primary">tambah</button>
-      </div>
+    <div v-for="item in products">
+      <CardMenus :item="item" @tambah="tambah" @kurang="kurang" />
     </div>
   </main>
 </template>
 
 <script>
 import NavbarCompVue from "@/components/NavbarComp.vue";
+import CardMenus from "@/components/CardMenus.vue";
 export default {
   name: "MenusView",
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          title: "Air Mineral",
+          desc: "500 ml",
+          price: 5400,
+          selected: 0,
+        },
+        {
+          id: 2,
+          title: "Salad Madu Cilacap",
+          desc: "Makanan khas cilacap",
+          price: 15000,
+          selected: 0,
+        },
+      ],
+    };
+  },
   components: {
     NavbarCompVue,
+    CardMenus,
+  },
+  methods: {
+    tambah(id) {
+      this.products.forEach((item) => {
+        if (item.id == id) {
+          item.selected++;
+        }
+      });
+    },
+    kurang(id) {
+      this.products.forEach((item) => {
+        if (item.id == id) {
+          item.selected = item.selected - 1;
+        }
+      });
+    },
   },
 };
 </script>
