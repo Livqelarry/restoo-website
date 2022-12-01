fb4527be0db017868ad229036cb2ade6f3f51d77
Praditya <pradityaaldi.s@gmail.com>
2022-12-02 01:28:35 +0700

    update card login

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,21 +3,29 @@
     <NavbarCompVue />
     <main class="py-5">
       <div class="container">
-        <h3 class="fw-bold">Silahkan login terlebih dahulu.</h3>
-        <p class="text-secondary">
-          Untuk pesan makanan, anda harus login terlebih dahulu.
-        </p>
-        <div class="d-flex flex-column gap-2 mb-4">
-          <label for="username">Username</label>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Masukkan username anda"
-            ref="username"
-          />
-        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <div class="card p-4">
+              <h3 class="fw-bold">Silahkan login terlebih dahulu.</h3>
+              <p class="text-secondary">
+                Untuk pesan makanan, anda harus login terlebih dahulu.
+              </p>
+              <div class="d-flex flex-column gap-2 mb-4">
+                <label for="username">Username</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Masukkan username anda"
+                  ref="username"
+                />
+              </div>
 
-        <button class="btn btn-primary" @click="handleLogin()">Login</button>
+              <button class="btn btn-primary" @click="handleLogin()">
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   </div>
